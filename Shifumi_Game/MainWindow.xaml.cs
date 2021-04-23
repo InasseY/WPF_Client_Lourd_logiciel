59f704832dc9c6308cc9a89284837d06184f1258
InasseY <inasse.youssouf@gmail.com>
2021-04-23 16:21:30 +0200

    shifumy game with %victory and defeat

---
+++ Shifumi_Game/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shifumi_Game
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string userChoice = ""; // Déclaration de mes variables et de leurs valeurs
+        string paper = "papier";
+        string stone = "pierre";
+        string cutter = "ciseau";
+        int userVictory = 0; // On commence à 0 pour indiquer que l'on commence une nouvelle partie
+        int userDefeat = 0;
+        int userSame = 0;
+        int totalGame = 0;
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            string percents = percentsTextBlock.Text; // Mon pourcentage de victoires
+            Random rnd = new Random(); // Génération d'une valeur prédéfinie au hasard
+            string[] iaChoices = { "pierre", "feuille", "ciseau" };
+            int choiceComputer = rnd.Next(iaChoices.Length); // Le choiceComputeur correspond à l'index de mon tableau
+
+            if (userChoice == "") // Si l'utilisateur ne rentre aucunes données
+            {
+                MessageBox.Show("Erreur, merci de sélectionner un des trois éléments ci-dessus");
+            }
+            else
+            {
+                if (userChoice == paper && iaChoices[choiceComputer] == stone || userChoice == stone && iaChoices[choiceComputer] == cutter || userChoice == cutter && iaChoices[choiceComputer] == paper)
+                {
+                    resultTextBlock.Text = "Gagné, tu as choisis : " + userChoice + " , et la machine : " + iaChoices[choiceComputer];
+                    userVictory++; // Ajouter les victoires pour les compter
+                    totalGame++; // Permet d'incrémenter mon total de parties pour calculer le % de victoires par la suite
+                }
+                else if (userChoice == paper && iaChoices[choiceComputer] == paper || userChoice == stone && iaChoices[choiceComputer] == stone || userChoice == cutter && iaChoices[choiceComputer] == cutter)
+                {
+                    resultTextBlock.Text = "Egalité, tu as choisis : " + userChoice + " , et la machine : " + iaChoices[choiceComputer];
+                    userSame++; // Ajouter les égalités pour les compter
+                    totalGame++;
+                }
+                else
+                {
+                    resultTextBlock.Text = "Perdu, tu as choisis : " + userChoice + " , et la machine : " + iaChoices[choiceComputer];
+                    userDefeat++; // Ajouter les défaites pour les compter
+                    totalGame++;
+                }
+                statsTextBlock.Text = "Nombre de victoire(s) : " + userVictory + " , \n Nombre de défaite(s) : " + userDefeat + " , \n Nombre d'égalité(s) : " + userSame + ".";
+                //counterTextBlock.Text = totalGame.ToString(); // Affichage dans mes textblocks de mes données receuillies au dessus
+                percentsTextBlock.Text = "Pourcentage de victoire(s) : \n" + 100 * userVictory / totalGame + " %";
+
+            }
+        }
+
+        private void Stone_Click(object sender, RoutedEventArgs e) // Méthode pour enregistrer la valeur au clique de l'élément sélectionné
+        {
+            userChoice = stone; // Lors du clique sur la pierre, la valeur sera stockée dans la variable userChoice
+        }
+
+        private void Paper_Click(object sender, RoutedEventArgs e)
+        {
+            userChoice = paper;
+        }
+
+        private void Cutter_Click(object sender, RoutedEventArgs e)
+        {
+            userChoice = cutter;
+        }
+    }
+}
+
